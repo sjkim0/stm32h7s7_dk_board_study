c4d8f2f0d600e4dd796065638bd1cbad865a9f20
ksj10111011@naver.com <ksj10111011@naver.com>
2026-01-07 18:09:43 +0900

    feat : hard fault

---
+++ _0_example_project/dk_example/Appli/Core/Src/i2s.c
@@ -0,0 +1,160 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    i2s.c
+  * @brief   This file provides code for the configuration
+  *          of the I2S instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "i2s.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+I2S_HandleTypeDef hi2s6;
+
+/* I2S6 init function */
+void MX_I2S6_Init(void)
+{
+
+  /* USER CODE BEGIN I2S6_Init 0 */
+
+  /* USER CODE END I2S6_Init 0 */
+
+  /* USER CODE BEGIN I2S6_Init 1 */
+
+  /* USER CODE END I2S6_Init 1 */
+  hi2s6.Instance = SPI6;
+  hi2s6.Init.Mode = I2S_MODE_MASTER_FULLDUPLEX;
+  hi2s6.Init.Standard = I2S_STANDARD_PHILIPS;
+  hi2s6.Init.DataFormat = I2S_DATAFORMAT_16B;
+  hi2s6.Init.MCLKOutput = I2S_MCLKOUTPUT_ENABLE;
+  hi2s6.Init.AudioFreq = I2S_AUDIOFREQ_8K;
+  hi2s6.Init.CPOL = I2S_CPOL_LOW;
+  hi2s6.Init.FirstBit = I2S_FIRSTBIT_MSB;
+  hi2s6.Init.WSInversion = I2S_WS_INVERSION_DISABLE;
+  hi2s6.Init.Data24BitAlignment = I2S_DATA_24BIT_ALIGNMENT_RIGHT;
+  hi2s6.Init.MasterKeepIOState = I2S_MASTER_KEEP_IO_STATE_DISABLE;
+  if (HAL_I2S_Init(&hi2s6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2S6_Init 2 */
+
+  /* USER CODE END I2S6_Init 2 */
+
+}
+
+void HAL_I2S_MspInit(I2S_HandleTypeDef* i2sHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  if(i2sHandle->Instance==SPI6)
+  {
+  /* USER CODE BEGIN SPI6_MspInit 0 */
+
+  /* USER CODE END SPI6_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SPI6;
+    PeriphClkInit.Spi6ClockSelection = RCC_SPI6CLKSOURCE_PCLK4;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* I2S6 clock enable */
+    __HAL_RCC_SPI6_CLK_ENABLE();
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOG_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**I2S6 GPIO Configuration
+    PB5     ------> I2S6_SDO
+    PG12     ------> I2S6_SDI
+    PA4     ------> I2S6_WS
+    PA3     ------> I2S6_MCK
+    PA5     ------> I2S6_CK
+    */
+    GPIO_InitStruct.Pin = I2S_SDO_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF8_SPI6;
+    HAL_GPIO_Init(I2S_SDO_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = I2S_SDI_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF5_SPI6;
+    HAL_GPIO_Init(I2S_SDI_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = I2S_WS_Pin|I2S_CK_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF8_SPI6;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = I2S_MCK_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF5_SPI6;
+    HAL_GPIO_Init(I2S_MCK_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN SPI6_MspInit 1 */
+
+  /* USER CODE END SPI6_MspInit 1 */
+  }
+}
+
+void HAL_I2S_MspDeInit(I2S_HandleTypeDef* i2sHandle)
+{
+
+  if(i2sHandle->Instance==SPI6)
+  {
+  /* USER CODE BEGIN SPI6_MspDeInit 0 */
+
+  /* USER CODE END SPI6_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SPI6_CLK_DISABLE();
+
+    /**I2S6 GPIO Configuration
+    PB5     ------> I2S6_SDO
+    PG12     ------> I2S6_SDI
+    PA4     ------> I2S6_WS
+    PA3     ------> I2S6_MCK
+    PA5     ------> I2S6_CK
+    */
+    HAL_GPIO_DeInit(I2S_SDO_GPIO_Port, I2S_SDO_Pin);
+
+    HAL_GPIO_DeInit(I2S_SDI_GPIO_Port, I2S_SDI_Pin);
+
+    HAL_GPIO_DeInit(GPIOA, I2S_WS_Pin|I2S_MCK_Pin|I2S_CK_Pin);
+
+  /* USER CODE BEGIN SPI6_MspDeInit 1 */
+
+  /* USER CODE END SPI6_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
---
+++ _0_example_project/dk_example/Appli/Core/Src/ucpd.c
@@ -0,0 +1,61 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    ucpd.c
+  * @brief   This file provides code for the configuration
+  *          of the UCPD instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "ucpd.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* UCPD1 init function */
+void MX_UCPD1_Init(void)
+{
+
+  /* USER CODE BEGIN UCPD1_Init 0 */
+
+  /* USER CODE END UCPD1_Init 0 */
+
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* Peripheral clock enable */
+  LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_UCPD1);
+
+  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOM);
+  /**UCPD1 GPIO Configuration
+  PM1   ------> UCPD1_CC2
+  PM0   ------> UCPD1_CC1
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_1|LL_GPIO_PIN_0;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOM, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN UCPD1_Init 1 */
+
+  /* USER CODE END UCPD1_Init 1 */
+  /* USER CODE BEGIN UCPD1_Init 2 */
+
+  /* USER CODE END UCPD1_Init 2 */
+
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
