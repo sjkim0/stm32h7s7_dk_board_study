e17af626c9be0682dd17564156248d09069e278a
ksj10111011@naver.com <ksj10111011@naver.com>
2026-01-21 10:02:22 +0900

    feat : ltdc 테스트중

---
+++ _1_workshop_test/_3_ext_mem_3/try_3/ExtMemLoader/Core/Src/xspi.c
@@ -0,0 +1,164 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    xspi.c
+  * @brief   This file provides code for the configuration
+  *          of the XSPI instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "xspi.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+XSPI_HandleTypeDef hxspi1;
+
+/* XSPI1 init function */
+void MX_XSPI1_Init(void)
+{
+
+  /* USER CODE BEGIN XSPI1_Init 0 */
+
+  /* USER CODE END XSPI1_Init 0 */
+
+  XSPIM_CfgTypeDef sXspiManagerCfg = {0};
+
+  /* USER CODE BEGIN XSPI1_Init 1 */
+
+  /* USER CODE END XSPI1_Init 1 */
+  hxspi1.Instance = XSPI1;
+  hxspi1.Init.FifoThresholdByte = 1;
+  hxspi1.Init.MemoryMode = HAL_XSPI_SINGLE_MEM;
+  hxspi1.Init.MemoryType = HAL_XSPI_MEMTYPE_MACRONIX;
+  hxspi1.Init.MemorySize = HAL_XSPI_SIZE_256MB;
+  hxspi1.Init.ChipSelectHighTimeCycle = 2;
+  hxspi1.Init.FreeRunningClock = HAL_XSPI_FREERUNCLK_DISABLE;
+  hxspi1.Init.ClockMode = HAL_XSPI_CLOCK_MODE_0;
+  hxspi1.Init.WrapSize = HAL_XSPI_WRAP_NOT_SUPPORTED;
+  hxspi1.Init.ClockPrescaler = 0;
+  hxspi1.Init.SampleShifting = HAL_XSPI_SAMPLE_SHIFT_NONE;
+  hxspi1.Init.ChipSelectBoundary = HAL_XSPI_BONDARYOF_NONE;
+  hxspi1.Init.MaxTran = 0;
+  hxspi1.Init.Refresh = 0;
+  hxspi1.Init.MemorySelect = HAL_XSPI_CSSEL_NCS1;
+  if (HAL_XSPI_Init(&hxspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sXspiManagerCfg.nCSOverride = HAL_XSPI_CSSEL_OVR_NCS1;
+  sXspiManagerCfg.IOPort = HAL_XSPIM_IOPORT_2;
+  sXspiManagerCfg.Req2AckTime = 1;
+  if (HAL_XSPIM_Config(&hxspi1, &sXspiManagerCfg, HAL_XSPI_TIMEOUT_DEFAULT_VALUE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN XSPI1_Init 2 */
+
+  /* USER CODE END XSPI1_Init 2 */
+
+}
+
+void HAL_XSPI_MspInit(XSPI_HandleTypeDef* xspiHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  if(xspiHandle->Instance==XSPI1)
+  {
+  /* USER CODE BEGIN XSPI1_MspInit 0 */
+
+  /* USER CODE END XSPI1_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_XSPI1;
+    PeriphClkInit.Xspi1ClockSelection = RCC_XSPI1CLKSOURCE_PLL2S;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* XSPI1 clock enable */
+    __HAL_RCC_XSPIM_CLK_ENABLE();
+    __HAL_RCC_XSPI1_CLK_ENABLE();
+
+    __HAL_RCC_GPION_CLK_ENABLE();
+    /**XSPI1 GPIO Configuration
+    PN1     ------> XSPIM_P2_NCS1
+    PN3     ------> XSPIM_P2_IO1
+    PN0     ------> XSPIM_P2_DQS0
+    PN11     ------> XSPIM_P2_IO7
+    PN10     ------> XSPIM_P2_IO6
+    PN9     ------> XSPIM_P2_IO5
+    PN2     ------> XSPIM_P2_IO0
+    PN6     ------> XSPIM_P2_CLK
+    PN8     ------> XSPIM_P2_IO4
+    PN4     ------> XSPIM_P2_IO2
+    PN5     ------> XSPIM_P2_IO3
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_3|GPIO_PIN_0|GPIO_PIN_11
+                          |GPIO_PIN_10|GPIO_PIN_9|GPIO_PIN_2|GPIO_PIN_6
+                          |GPIO_PIN_8|GPIO_PIN_4|GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF9_XSPIM_P2;
+    HAL_GPIO_Init(GPION, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN XSPI1_MspInit 1 */
+
+  /* USER CODE END XSPI1_MspInit 1 */
+  }
+}
+
+void HAL_XSPI_MspDeInit(XSPI_HandleTypeDef* xspiHandle)
+{
+
+  if(xspiHandle->Instance==XSPI1)
+  {
+  /* USER CODE BEGIN XSPI1_MspDeInit 0 */
+
+  /* USER CODE END XSPI1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_XSPIM_CLK_DISABLE();
+    __HAL_RCC_XSPI1_CLK_DISABLE();
+
+    /**XSPI1 GPIO Configuration
+    PN1     ------> XSPIM_P2_NCS1
+    PN3     ------> XSPIM_P2_IO1
+    PN0     ------> XSPIM_P2_DQS0
+    PN11     ------> XSPIM_P2_IO7
+    PN10     ------> XSPIM_P2_IO6
+    PN9     ------> XSPIM_P2_IO5
+    PN2     ------> XSPIM_P2_IO0
+    PN6     ------> XSPIM_P2_CLK
+    PN8     ------> XSPIM_P2_IO4
+    PN4     ------> XSPIM_P2_IO2
+    PN5     ------> XSPIM_P2_IO3
+    */
+    HAL_GPIO_DeInit(GPION, GPIO_PIN_1|GPIO_PIN_3|GPIO_PIN_0|GPIO_PIN_11
+                          |GPIO_PIN_10|GPIO_PIN_9|GPIO_PIN_2|GPIO_PIN_6
+                          |GPIO_PIN_8|GPIO_PIN_4|GPIO_PIN_5);
+
+  /* USER CODE BEGIN XSPI1_MspDeInit 1 */
+
+  /* USER CODE END XSPI1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
